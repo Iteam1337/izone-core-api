54f7789848cd06a656bf177dbeb278e9225ac841
ilix <src@ilix.se>
2017-06-21 14:57:26 +0200

    Rename default ValuesController to SlackController.

---
+++ Izone.API/Controllers/SlackController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Izone.API.Controllers
+{
+    [Route("[controller]")]
+    public class SlackController : Controller
+    {
+        // POST api/values
+        [HttpPost]
+        public dynamic Post([FromBody]string value)
+        {
+            return new {
+                text = "testing"
+            };
+        }
+    }
+}

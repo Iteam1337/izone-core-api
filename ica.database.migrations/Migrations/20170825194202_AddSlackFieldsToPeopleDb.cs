f3f952b73f6af742e1196d372613efc82cca94fe
Alexander Czigler <src@ilix.se>
2017-08-25 21:44:45 +0200

    Created migration for adding slack fields to people_db.

---
+++ ica.database.migrations/Migrations/20170825194202_AddSlackFieldsToPeopleDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ica.database.migrations.Migrations
+{
+    public partial class AddSlackFieldsToPeopleDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>("p_slack_id", "people_db");
+            migrationBuilder.AddColumn<string>("p_slack_username", "people_db");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn("p_slack_id", "people_db");
+            migrationBuilder.DropColumn("p_slack_username", "people_db");
+        }
+    }
+}

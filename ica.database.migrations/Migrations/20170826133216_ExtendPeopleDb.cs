ef08f86315e3efa30a81bd7d2990927740bfa541
Alexander Czigler <src@ilix.se>
2017-08-26 15:52:59 +0200

    Created migrations for extending people_db and seeding with a person.

---
+++ ica.database.migrations/Migrations/20170826133216_ExtendPeopleDb.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ica.database.migrations.Migrations
+{
+    public partial class ExtendPeopleDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>("p_firstname", "people_db");
+            migrationBuilder.AddColumn<string>("p_lastname", "people_db");
+            migrationBuilder.AddColumn<string>("p_izusername", "people_db");
+            migrationBuilder.AddColumn<string>("p_email", "people_db");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn("p_firstname", "people_db");
+            migrationBuilder.DropColumn("p_lastname", "people_db");
+            migrationBuilder.DropColumn("p_izusername", "people_db");
+            migrationBuilder.DropColumn("p_email", "people_db");
+        }
+    }
+}
---
+++ ica.database.migrations/Migrations/20170826133347_SeedPeopleDb.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ica.database.migrations.Migrations
+{
+    public partial class SeedPeopleDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO people_db
+                (
+                     p_id
+                    ,p_firstname
+                    ,p_lastname
+                    ,p_izusername
+                    ,p_email
+                    ,p_slack_id
+                    ,p_slack_username
+                )
+                VALUES
+                (
+                     1
+                    ,'Kisse'
+                    ,'Katt'
+                    ,'cat'
+                    ,'cat@iteam.se'
+                    ,'U0260BV6S'
+                    ,'ilix'
+                )
+            "
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM people_db
+                WHERE p_id = 1
+            "
+            );
+        }
+    }
+}
